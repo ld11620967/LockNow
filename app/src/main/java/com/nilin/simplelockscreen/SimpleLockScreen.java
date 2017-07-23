ca562e695c85a3cdfa24221b22e2864e0955bbcd
ld11620967 <11620967@qq.com>
2017-07-23 11:11:53 +0800

    updata icon

--- app/src/main/java/com/nilin/simplelockscreen/SimpleLockScreen.java
+++ app/src/main/java/com/nilin/simplelockscreen/SimpleLockScreen.java
@@ -15,36 +15,19 @@ public class SimpleLockScreen extends Activity{
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //获取设备管理服务
         policyManager = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
-        //AdminReceiver 继承自 DeviceAdminReceiver
         componentName = new ComponentName(this, AdminReceiver.class);
-        mylock();
-        finish();
-    }
-
-
-    private void mylock(){
-
         boolean active = policyManager.isAdminActive(componentName);
-        if(!active){//若无权限
-            activeManage();//去获得权限
+        if(!active){
+            Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+            intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, componentName);
+            startActivityForResult(intent, 0);
         }
         if (active) {
-            policyManager.lockNow();//直接锁屏
+            policyManager.lockNow();
         }
+        finish();
     }
-
-    private void activeManage() {
-        // 启动设备管理(隐式Intent) - 在AndroidManifest.xml中设定相应过滤器
-        Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
-        //权限列表
-        intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, componentName);
-        //描述(additional explanation)
-//        intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "------ 需以下权限 ------");
-        startActivityForResult(intent, 0);
-    }
-
 }
 
 
